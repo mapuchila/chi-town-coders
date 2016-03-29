ddca1a14452e3fae0f4c92f2cb841c3d8f79d813
Marcin aka Sin <marcinmu330@gmail.com>
2016-03-29 16:53:54 -0500

    replaced entire src directory with finished code

--- src/app.js
+++ src/app.js
@@ -1,45 +1,43 @@
 'use strict';
 
 var express = require('express'),
-	posts = require('./mock/posts.json');
-var postList = Object.keys(posts).map(function(value){ 
-		return posts[value];
-	});
+	  posts = require('./mock/posts.json');
+
+var postsLists = Object.keys(posts).map(function(value) {
+							         return posts[value]})
 
 var app = express();
-// app.use adds middleware to the app.
-app.use('/static', express.static(__dirname + '/public'));
-// app.set adds app variables
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/views');
 
-app.get('/', function(request, response) {
-	var path = request.path;
-	console.log(path);
-	response.locals.path = path;
-	response.render('index');
+app.use('/static', express.static(__dirname + '/public'))
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/templates');
+
+app.get('/', function(req, res){
+	var path = req.path;
+	res.locals.path = path;
+	res.render('index');
 });
 
-app.get('/blog/:title?', (req, res) => {
-
+app.get('/blog/:title?', function(req, res){ 
 	var title = req.params.title;
 	if (title === undefined) {
 		res.status(503);
-		res.render('blog', {posts: postList});
+		res.render('blog', {posts: postsLists})
 	} else {
 		var post = posts[title] || {};
-		res.render('post', {post: post});
+		res.render('post', { post: post});
 	}
 });
 
 app.get('/posts', function(req, res) {
-	if(req.query.raw) {
+	if (req.query.raw) {
 		res.json(posts);
 	} else {
-		res.json(postList);
+		res.json(postsLists);
 	}
-});
+})
 
 app.listen(3000, function() {
-	console.log("The front-end server is running on port 3000!");
+	console.log("The frontend server is running on port 3000!");
 });
