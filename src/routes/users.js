172670db6e8dacd217b678eab50c28d92c1f557c
Marcin aka Sin <marcinmu330@gmail.com>
2016-04-19 17:22:11 -0500

    added some registration validation

--- src/routes/users.js
+++ src/routes/users.js
@@ -9,7 +9,44 @@ router.get('/sign-up', function(req, res){
 	res.render('users/sign-up');
 });
 
+router.post('/sign-up', function(req, res){
+	var path = req.path;
+	res.locals.path = path;
+
+	var firstName = req.body['txt-first-name'];
+	var lastName = req.body['txt-last-name'];
+	var email = req.body['txt-email'];
+	var password = req.body['txt-password'];
+	var confirmPassword = req.body['txt-password-confirm'];
+	console.log(Object.keys(req.body));
+
+	req.checkBody('txt-first-name', 'First Name is Required.').notEmpty();
+	req.checkBody('txt-last-name', 'Last Name is Required.').notEmpty();
+	req.checkBody('txt-email', 'Email is Required.').notEmpty();
+	req.checkBody('txt-email', 'Email is not valid.').isEmail();
+	req.checkBody('txt-password', 'Password is Required.').notEmpty();
+	req.checkBody('txt-password-confirm', 'Passwords do not match!').equals(req.body['txt-password']);
+
+	var errors = req.validationErrors();
+	//
+	if(errors) {
+		console.log(errors);
+		res.render('users/sign-up', {
+			errors: errors
+		});
+	} else {
+		res.render('index');
+	}
+});
+
 router.get('/sign-in', function(req, res){
+	var path = req.path;
+	res.locals.path = path;
+	//console.log(path);
+	res.render('users/sign-in');
+});
+
+router.post('/sign-in', function(req, res){
 	var path = req.path;
 	res.locals.path = path;
 	//console.log(path);
